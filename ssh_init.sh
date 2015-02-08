edac6b82418d3389945ea6dbc4416b3580b51ab9
FayeHuang <faye1821@gmail.com>
2015-02-08 21:59:39 +0800

    add ssh setup script. include change root password and start ssh daemon

---
+++ ssh_init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DEFAULT_ROOTPASSWORD=changeme
+if [ ${ROOTPASSWORD} ]; then
+  echo "root:$ROOTPASSWORD" | chpasswd
+else
+  echo "root:$DEFAULT_ROOTPASSWORD" | chpasswd
+fi
+
+exec /usr/sbin/sshd -D
